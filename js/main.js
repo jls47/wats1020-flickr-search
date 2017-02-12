4644b1584dc39489a67cbd827baa01a2dffbf8ff
jls47 <jls47@users.noreply.github.com>
2017-02-11 23:20:36 -0800

    Commit with just about all stretch goals!

--- js/main.js
+++ js/main.js
@@ -27,7 +27,9 @@ $(document).ready(function(){
         //
         // 4.   Update the display to add the images to the list with the id
         //      `#images`.
+	var listLen = 0;
 	var searchImages = function(tags){
+		$("#images").empty();
 		var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 			$.getJSON( flickerAPI, {
 			tags: tags,
@@ -36,8 +38,37 @@ $(document).ready(function(){
 		})
 		.done(function( data ) {
 			$.each( data.items, function( i, item ) {
-				$( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
-			if ( i === 30 ) {
+				
+				var newImgList = $("<li>");
+				//create css with jquery
+				$( "<img>" ).attr( "src", item.media.m ).appendTo(newImgList);
+				/* var title = $('<p class="image-title">').html(item.title).appendTo(newImgList);
+				var date = $('<p class = "image-date">').text(item.date_taken).appendTo(newImgList);
+				var desc = $("<p class = 'image-description'>").html(item.description).appendTo(newImgList);
+				var Link = $("<a>").attr('href', item.link).text("Check this out on Flickr!").appendTo(newImgList); */
+				$("<br>").appendTo(newImgList);
+				var newButton = $("<button class='btn btn-sm btn-primary'><span class='glyphicon glyphicon-zoom-in'></span> enlarge</button>").attr({
+					'data-title': item.title,
+					'data-toggle': "modal",
+					'data-target': "#infoModal",
+					'data-imgsrc': item.media.m,
+					'data-author': item.author,
+					'data-description': item.description,
+					'type': "button"
+				}).appendTo(newImgList);
+				
+				$("<br><br><br>").appendTo(newImgList);
+				
+				newImgList.appendTo("#images");
+				
+				$("#images").css("margin", "0 auto");
+				$("#images").css("text-align", "center");
+				$("#images").css("background", "rgba(0, 0, 0, 0.0)")
+				$("#images").css("border", "none")
+				$("#images li").css("display", "inline-block");
+				$("#images li").css("width", "300px");
+				
+			if ( i === 19 ) {
 				return false;
 			}
 			});
@@ -59,18 +90,38 @@ $(document).ready(function(){
         //      user.
 		
 		//var tags = "asd";
-	$("button.search").click(function(e){
+	var imgURL = [];
+	$("button.search").click(function(e, tags){
 		e.preventDefault();
-		$("#images").empty();
-		tags = $('.form-control').val();
-		console.log(tags);
-		searchImages(tags);
-	});
 		
+		tags = $('.form-control').val();
+		if(tags.length  < 1){
+			alert("Put some text to search, ya dingus!");
+			return;
+		};
+		console.log(tags);
+		//$("#images").empty();
+		$("h1.search-title").first()[0].innerHTML = "Found " + tags + " via Flickr";
+		searchImages(tags);
+		
+	});
 		
     // STRETCH GOAL: Add a "more info" popup using the technique shown on the
     // Bootstrap Modal documentation: http://getbootstrap.com/javascript/#modals-related-target
-
-
+	
+	$('#infoModal').on('show.bs.modal', function (ev) {
+		var button = $(ev.relatedTarget);
+		var title = button.data('title');
+		var imgSrc = button.data('imgsrc');
+		var imgAuthor = button.data('author');
+		var imgDescription = button.data('description');
+		
+		var modal = $(this);
+		modal.find('.modal-title').empty();
+		modal.find('.modal-body').empty();
+		
+		modal.find('.modal-title').html("<h2>" + title + "</h2>");
+		modal.find('.modal-body').html("<h3> Image by " + imgAuthor + "</h3>" + "<br>" + imgDescription);
+	});
 
 });
