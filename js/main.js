44b1ac2db77866cc89218ed1625a1474bcdef7b0
jls47 <jls47@users.noreply.github.com>
2017-02-12 22:44:57 -0800

    Who has two thumbs and forgot comments the first time?

--- js/main.js
+++ js/main.js
@@ -27,26 +27,27 @@ $(document).ready(function(){
         //
         // 4.   Update the display to add the images to the list with the id
         //      `#images`.
-	var listLen = 0;
+	//Starting with the searchImages function, which takes a string "tags" as an argument.
 	var searchImages = function(tags){
+		//First, we empty the page of the previous results so it doesn't become an unparseable mess.
 		$("#images").empty();
+		//Next, we load the API into the variable flickerAPI, with the search term "tags".
 		var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 			$.getJSON( flickerAPI, {
 			tags: tags,
 			tagmode: "any",
 			format: "json"
 		})
+		//When that's done, we create new list elements for each image.  This is where it gets hairy.
 		.done(function( data ) {
 			$.each( data.items, function( i, item ) {
 				
 				var newImgList = $("<li>");
-				//create css with jquery
+				//To this list element we append the image from "src" in the JSON data.
 				$( "<img>" ).attr( "src", item.media.m ).appendTo(newImgList);
-				/* var title = $('<p class="image-title">').html(item.title).appendTo(newImgList);
-				var date = $('<p class = "image-date">').text(item.date_taken).appendTo(newImgList);
-				var desc = $("<p class = 'image-description'>").html(item.description).appendTo(newImgList);
-				var Link = $("<a>").attr('href', item.link).text("Check this out on Flickr!").appendTo(newImgList); */
+				//This keeps the modal button below the image itself.
 				$("<br>").appendTo(newImgList);
+				//Creating the modal button!  Here we attach all the elements we need from the JSON data, from the title to the description.
 				var newButton = $("<button class='btn btn-sm btn-primary'><span class='glyphicon glyphicon-zoom-in'></span> enlarge</button>").attr({
 					'data-title': item.title,
 					'data-toggle': "modal",
@@ -56,11 +57,11 @@ $(document).ready(function(){
 					'data-description': item.description,
 					'type': "button"
 				}).appendTo(newImgList);
-				
+				//Putting line breaks in between each image element.
 				$("<br><br><br>").appendTo(newImgList);
-				
+				//Now we actually append each list element to the ul in the HTML!
 				newImgList.appendTo("#images");
-				
+				//Modifying the CSS so the white background disappears and forms a grid.
 				$("#images").css("margin", "0 auto");
 				$("#images").css("text-align", "center");
 				$("#images").css("background", "rgba(0, 0, 0, 0.0)")
@@ -69,6 +70,7 @@ $(document).ready(function(){
 				$("#images li").css("width", "300px");
 				
 			if ( i === 19 ) {
+				//Limiting the images to 20 per page.
 				return false;
 			}
 			});
@@ -90,17 +92,20 @@ $(document).ready(function(){
         //      user.
 		
 		//var tags = "asd";
-	var imgURL = [];
+	//Making the search button work!
 	$("button.search").click(function(e, tags){
+		//First we stop what the button usually does.
 		e.preventDefault();
-		
+		//Then, we grab the var tags from the form the user filled out (the search bar).
 		tags = $('.form-control').val();
+		//If they didn't enter anything remind them to do so.
 		if(tags.length  < 1){
 			alert("Put some text to search, ya dingus!");
 			return;
 		};
-		console.log(tags);
-		//$("#images").empty();
+		//Just making sure the form worked.
+		//console.log(tags);
+		//Update the page header accordingly with the tags and success of the search, then execute the search function.
 		$("h1.search-title").first()[0].innerHTML = "Found " + tags + " via Flickr";
 		searchImages(tags);
 		
@@ -108,18 +113,21 @@ $(document).ready(function(){
 		
     // STRETCH GOAL: Add a "more info" popup using the technique shown on the
     // Bootstrap Modal documentation: http://getbootstrap.com/javascript/#modals-related-target
-	
+	//Modal code!  Brings up the window.
 	$('#infoModal').on('show.bs.modal', function (ev) {
+		//Secondary target for the event, in this case closing the modal window.
 		var button = $(ev.relatedTarget);
+		//Gathering the JSON data to display in the modal window.
 		var title = button.data('title');
 		var imgSrc = button.data('imgsrc');
 		var imgAuthor = button.data('author');
 		var imgDescription = button.data('description');
-		
+		//Displaying all the info!
 		var modal = $(this);
+		//Erasing the defaults
 		modal.find('.modal-title').empty();
 		modal.find('.modal-body').empty();
-		
+		//And adding the found flickr values!
 		modal.find('.modal-title').html("<h2>" + title + "</h2>");
 		modal.find('.modal-body').html("<h3> Image by " + imgAuthor + "</h3>" + "<br>" + imgDescription);
 	});
